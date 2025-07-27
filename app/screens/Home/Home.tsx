ddd0ac48e2ba1a695bf2b39a50335c3e4fe889c5
jali Mnisi <jalimnisi@MacBookAir.home>
2025-07-28 00:01:18 +0200

    Global colors

--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -4,14 +4,18 @@ import React from 'react';
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { HomeStackParamList } from '../../navigation/HomeStack';
+import globalColors from '../../../assets/globalstyle'; // Import global colors
 
 type Props = NativeStackScreenProps<HomeStackParamList, 'HomeMain'>;
 
 export default function Home({ navigation }: Props) {
+
+// global colors
+
+
+
+
   return (
-
-
-
 <>
             <View style={styles.top}>
                 <Image
@@ -26,50 +30,43 @@ export default function Home({ navigation }: Props) {
 
             
                 <Pressable onPress={() => navigation.navigate('Logger')}>
-                    <View style={styles.cards}>
-                    <Icon name="list" style={styles.iconsize} />
-                        <Text style={styles.textsize}>Shift Logger</Text>
+                    <View style={[styles.cards, styles.loggershadow]}>
+                    <Icon name="list" style={styles.logger} />
+                        <Text style={styles.loggertextsize}>Shift Logger</Text>
 
                     </View>
                 </Pressable>
                 
                 <Pressable onPress={() => navigation.navigate('Claim')}>
-                <View style={styles.cards}>
-                    <Icon name="car-sport" style={styles.iconsize} />
-                      <Text style={styles.textsize}>Claim</Text>
+                <View style={[styles.cards, styles.claimshadow]}>
+                    <Icon name="car-sport" style={styles.claim} />
+                      <Text style={styles.claimtextsize}>Claim</Text>
                 </View>
                 </Pressable>
 
 <               Pressable onPress={() => navigation.navigate('Notification')}>
-                <View style={styles.cards}>
-                    <Icon name="notifications" style={styles.iconsize} />
-                    <Text style={styles.textsize}>Notifications </Text>
+                <View style={[styles.cards, styles.notifyshadow]}>
+                    <Icon name="notifications" style={styles.notify} />
+                    <Text style={styles.notifytextsize}>Notifications </Text>
                 </View>
                 </Pressable>
                 
                 <Pressable onPress={() => navigation.navigate('Issue')}>
-                <View style={styles.cards}>
-                    <Icon name="help-circle" style={styles.iconsize} />
-                    <Text style={styles.textsize}> Issue </Text>
+                <View style={[styles.cards, styles.issueshadow]}>
+                    <Icon name="help-circle" style={styles.issue} />
+                    <Text style={styles.issuetextsize}> Issue </Text>
                 </View>
 
                 </Pressable>
             </View>
         
-        </>
-
-
-
-    // <View style={styles.container}>
-    //   <Button title="Go to Claim" onPress={() => navigation.navigate('Claim')} />
-    //   <Button title="Go to Issue" onPress={() => navigation.navigate('Issue')} />
-    //   <Button title="Go to Logger" onPress={() => navigation.navigate('Logger')} />
-    //   <Button title="Go to Notification" onPress={() => navigation.navigate('Notification')} />
-    // </View>
+</>
   );
 }
 
 const styles = StyleSheet.create({
+
+    
   container: {
     flex: 1,
     height: '100%',
@@ -110,20 +107,75 @@ cards:{
         textAlign: 'center',
         justifyContent: 'center',
         alignItems: 'center',
-        shadowColor: '#005f6c', // blue shadow color
+        
+    },
+    logger: {
+        fontSize: 40,
+        color: globalColors.loggercolor, // steel blue color
+    }, 
+    loggertextsize: {
+        fontSize: 10,
+        fontWeight: 'bold',
+        color: globalColors.loggercolor , // steel blue color 
+    },   
+    issue: {
+        fontSize: 40,
+        color: globalColors.Issuecolor, // blue color
+    },
+     issuetextsize: {
+        fontSize: 10,
+        fontWeight: 'bold',
+        color: '#4682B4', 
+    },    
+    claim: {
+        fontSize: 40,
+        color: globalColors.claimcolor, // purple color
+    }, 
+     claimtextsize: {
+        fontSize: 10,
+        fontWeight: 'bold',
+        color: globalColors.claimcolor, // purple color
+    },   
+    notify: {
+        fontSize: 40,
+        color: globalColors.notificationcolor, // yellow color
+    },
+     notifytextsize: {
+        fontSize: 10,
+        fontWeight: 'bold',
+        color: globalColors.notificationcolor, // yellow color
+    },
+
+    // card shadows
+    notifyshadow : {
+        shadowColor: globalColors.notificationcolor, // yellow shadow color
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 1,
         shadowRadius: 10,
         elevation: 5,
     },
-    textsize: {
-        fontSize: 10,
-        fontWeight: 'bold',
-        color: '#4682B4', // steel blue color 
+    claimshadow : {
+        shadowColor: globalColors.claimcolor, // purple shadow color
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        elevation: 5,
     },
-    iconsize: {
-        fontSize: 40,
-        color: '#89CFF0', // steel blue color
+    issueshadow : {
+        shadowColor: globalColors.Issuecolor, // blue shadow color
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        elevation: 5,
+    },
+    loggershadow : {
+        shadowColor: globalColors.loggercolor, // blue shadow color
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        elevation: 5,
     }
+
+
   }   
 );
---
+++ assets/globalstyle.ts
@@ -0,0 +1,14 @@
+  const globalColors = {
+
+  Issuecolor: '#89CFF0',
+  notificationcolor: '#FFC107',
+  iconRed: '#FF6F61',
+  loggercolor: '#4CAF50',
+  claimcolor: '#673AB7',
+
+  Primary : '#6200EE',
+ Secondary : '#03DAC6',
+
+  }
+
+  export default globalColors ;
